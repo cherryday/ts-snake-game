528a92ef860e663c6e770bbec574af4bead602b4
cherryday <tcarevmaxim@gmail.com>
2021-08-10 23:26:14 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.ts',
+  output: {
+    clean: true,
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: 'dist',
+    port: 3000,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    })
+  ]
+}
