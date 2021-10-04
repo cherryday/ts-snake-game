5136b093b39695c530877096c8e29534e02403ca
cherryday <tcarevmaxim@gmail.com>
2021-10-04 23:55:31 +0300

    test Food class

---
+++ tests/snake/food.spec.ts
@@ -0,0 +1,22 @@
+import { Food } from '../../src/snake/Food'
+import { Cell, CellContent } from '../../src/snake/Cell'
+
+describe('Food', () => {
+  it('should set cell content when init', () => {
+    const cell = new Cell(0, 0)
+    const food = new Food(cell)
+    
+    expect(cell.getContent).toBe(CellContent.FOOD)
+    expect(food.getCell.getContent).toBe(CellContent.FOOD)
+  })
+
+  it('should set cell content when change cell', () => {
+    const cell = new Cell(0, 0)
+    const nextCell = new Cell(0, 0)
+    const food = new Food(cell)
+    food.setCell = nextCell
+
+    expect(cell.getContent).toBe(CellContent.EMPTY)
+    expect(nextCell.getContent).toBe(CellContent.FOOD)
+  })
+})
